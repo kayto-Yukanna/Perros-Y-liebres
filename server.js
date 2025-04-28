6bf651a850d10e98b2e91074b475555d4444b196
kayto-Yukanna <147193937+kayto-Yukanna@users.noreply.github.com>
2025-04-27 23:39:45 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = new Server(server, {
+  cors: { origin: "*", methods: ["GET", "POST"] },
+});
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
-const rooms = {}; // Guardar el estado de cada sala
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+let rooms = {};  // Guardar el estado de cada sala
 
 io.on("connection", (socket) => {
     console.log("Un jugador se ha conectado");
